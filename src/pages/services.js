82d2553d2c3868398bd95ffd59d14067df39a998
mrburma <mrburma@localhost.localdomain>
2021-06-11 17:49:00 +0630

    Add Service Read More Buttons

--- src/pages/services.js
+++ src/pages/services.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Container, Grid, Heading, Text, Image } from "theme-ui";
+import { Box, Container, Grid, Heading, Text, Image, Button } from "theme-ui";
 import BlockTitle from "components/block-title";
 
 import icon1 from "assets/icons/service-1-1.svg";
@@ -8,6 +8,7 @@ import icon3 from "assets/icons/service-1-3.svg";
 import icon4 from "assets/icons/service-1-4.svg";
 import icon5 from "assets/icons/service-1-5.svg";
 import image1 from "assets/services.svg";
+import { Link } from "components/link";
 
 const SERVICES_DATA = [
   {
@@ -41,15 +42,16 @@ const Services = () => {
   return (
     <Box sx={styles.services}>
       <Container>
-        <Image src={image1}/>
-      <BlockTitle slogan="" title="Services" styles={styles.blockTitle} />
+        <Image src={image1} />
+        <BlockTitle slogan="" title="Services" styles={styles.blockTitle} />
         <Text as="p">
-        Anahata is a competent provider of any kind of software solutions including
-mission critical business applications, complex engineering software, data centric analytical
-software, smart device solutions, web solutions, eCommerce solutions, Cyber Security solutions
-and so on. Our expertise lies in clearly perceiving the business goals of our customers,
-choosing the most appropriate technologies and efficiently developing solutions catered to the
-business needs.
+          Anahata is a competent provider of any kind of software solutions
+          including mission critical business applications, complex engineering
+          software, data centric analytical software, smart device solutions,
+          web solutions, eCommerce solutions, Cyber Security solutions and so
+          on. Our expertise lies in clearly perceiving the business goals of our
+          customers, choosing the most appropriate technologies and efficiently
+          developing solutions catered to the business needs.
         </Text>
 
         <Grid sx={styles.grid}>
@@ -59,9 +61,10 @@ business needs.
               sx={styles.serviceCard}
               key={`service-post-${index}`}
             >
-              <Image src={service.icon} alt=""/>
-              <Heading as="h2">{service.title}</Heading>
-              <Text as="p">{service.text}</Text>
+              <Image src={service.icon} alt="" />
+              {/* <Heading as="h3">{service.title}</Heading> */}
+              <Link sx={styles.links} path="/services">{service.title}</Link>
+              <Button as="p" variant="primary">Read More</Button>
             </Box>
           ))}
         </Grid>
@@ -76,21 +79,22 @@ const styles = {
   services: {
     h3: {
       margin: 0,
-      color: 'black',
-      fontSize: [5, null, null, '21px', '36px', '32px', 8],
-      letterSpacing: ['-0.5px', null, null, null, null, '-1.5px'],
-      fontWeight: 'bold',
+      color: "black",
+      fontSize: [5, null, null, "21px", "36px", "32px", 8],
+      letterSpacing: ["-0.5px", null, null, null, null, "-1.5px"],
+      fontWeight: "bold",
       lineHeight: [1.6, null, null, 1.25],
       textAlign: "center",
     },
     p: {
-      fontSize: ['15px', null, null, '17px'],
-      lineHeight: ['26px', null, null, 2],
-      mt: ['10px', null, null, null, '25px'],
-      mb: ['50px', null, null, null, '120px'],
+      color:"#FFFFFF",
+      fontSize: ["15px", null, null, "17px"],
+      lineHeight: ["26px", null, null, 2],
+      mt: ["10px", null, null, null, "25px"],
+      mb: ["50px", null, null, null, "120px"],
     },
     br: {
-      display: ['none', null, null, 'inherit'],
+      display: ["none", null, null, "inherit"],
     },
     pt: ["130px", null, null, null, null, null, "120px"],
   },
@@ -107,7 +111,6 @@ const styles = {
       "1fr 1fr",
       null,
       "1fr 1fr 1fr 1fr 1fr",
-      
     ],
   },
   serviceCard: {
@@ -129,5 +132,8 @@ const styles = {
       maxWidth: [null, null, null, null, "84%", "100%"],
       mx: [null, null, null, null, "auto", "0"],
     },
+    // links: {
+    //   backgroundColor : "primary"
+    // }
   },
 };
